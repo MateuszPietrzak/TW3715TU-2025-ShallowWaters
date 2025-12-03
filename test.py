7e834e61713f954f45361343f804b52d2ff0b960
federicoangioni <f.angioni@student.tudelft.nl>
2025-12-03 16:30:29 +0100

    changes on HB

--- test.py
+++
@@ -1,9 +0,0 @@
-from sympy import symbols, cos, fourier_series, pi
-
-x = symbols('x')
-f = cos(x)**8
-
-# Compute Fourier series over the interval [-pi, pi]
-fs = fourier_series(f, (x, -pi, pi))
-
-print(fs.truncate(n=10))
