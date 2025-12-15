23d0d6dc2866053ec5f9bdd2b4ce44147488fa06
federicoangioni <f.angioni@student.tudelft.nl>
2025-12-15 12:26:02 +0100

    Implement HB with functions in 1D and 2D

--- harmonicBalance/multiharmonic_balance.jl
+++ harmonicBalance/multiharmonic_balance.jl
@@ -7,41 +7,72 @@ using DomainSets
 import ApproxFun as AF
 import DifferentialEquations as DE
 
-function create_ansatz(x, t, omega, harmonics)
+function create_ansatz(coords::Tuple, t, omega, harmonics)
     u = 0
     j = 1
-    vars = []      # Will store callable functions like A, B, C...
-    var_exprs = [] # Will store expressions like A(x), B(x), C(x)...
+    vars = []
+    var_exprs = []
     
     for i in 1:(2*harmonics)
         c = Char('A' + i - 1)
         v = eval(:(Model.@variables $(Symbol(c))(..)))
         
         if isodd(i)
-            u += v[1](x) * sin(j * omega * t)
+            u += v[1](coords...) * sin(j * omega * t)
         else
-            u += v[1](x) * cos(j * omega * t)
+            u += v[1](coords...) * cos(j * omega * t)
             j += 1
         end
         
-        push!(vars, v[1])        # Store the callable function
-        push!(var_exprs, v[1](x)) # Store the expression with x
+        push!(vars, v[1])
+        push!(var_exprs, v[1](coords...))
     end
     
     return vars, var_exprs, u
 end
 
-function create_bcs(vars, domain, bc)
-    xleft, xright = domain
+function create_bcs(vars, domains, indep_vars, bc)
+    # domains: tuple of tuples, e.g., ((xleft, xright),) for 1D or ((xleft, xright), (yleft, yright)) for 2D
+    # indep_vars: tuple of independent variables, e.g., (x,) or (x, y)
+    # vars: the callable functions (A, B, C, ...)
+    # bc: boundary condition value
+    
+    ndims = length(domains)
     bcs = []
-    for var in vars
-        push!(bcs, var(xleft) ~ bc)  # Now var is callable
-        push!(bcs, var(xright) ~ bc)
+    
+    if ndims == 1
+        x = indep_vars[1]
+        xleft, xright = domains[1]
+        for var in vars
+            push!(bcs, var(xleft) ~ bc)
+            push!(bcs, var(xright) ~ bc)
+        end
+    elseif ndims == 2
+        x, y = indep_vars
+        xleft, xright = domains[1]
+        yleft, yright = domains[2]
+        for var in vars
+            push!(bcs, var(xleft, y) ~ bc)
+            push!(bcs, var(xright, y) ~ bc)
+            push!(bcs, var(x, yleft) ~ bc)
+            push!(bcs, var(x, yright) ~ bc)
+        end
     end
+    
     return bcs
 end
 
-function expand_trig(eqn, x, t, omega, verbose=false)
+# function create_bcs(vars, domain, bc)
+#     xleft, xright = domain
+#     bcs = []
+#     for var in vars
+#         push!(bcs, var(xleft) ~ bc)  # Now var is callable
+#         push!(bcs, var(xright) ~ bc)
+#     end
+#     return bcs
+# end
+
+function expand_trig(eqn, t, omega, verbose=false)
     """
     The idea for this new implementation is to use the form of the PDE with the ansatz substituted in 
     and extract each term (separate wrt addition/subtraction)
@@ -55,7 +86,6 @@ function expand_trig(eqn, x, t, omega, verbose=false)
     symbolics_list = Symb.arguments(y_exp, +);
     
     t_sympy = sp.symbols("t")
-    x_sympy = sp.symbols("x")
     finished_terms = []  # Initialize empty list
     
     for (i, term) in enumerate(symbolics_list)
@@ -94,33 +124,66 @@ end
 
 
 
-function solve_harmonicbalance(eqs, harmonics, bcs, domains, stepx, vars, vars_symb, x0)
-    xleft, xright = domains
-    domains = [x ∈ Interval(xleft, xright)];
+# function solve_harmonicbalance(eqs, harmonics, bcs, domains, stepx, vars, vars_symb, x0)
+#     xleft, xright = domains
+#     domains = [x ∈ Interval(xleft, xright)];
 
-    # Create the PDESystem
-    Model.@named pdesys = Model.PDESystem(eqs, bcs, domains, [vars], vars_symb);
+#     # Create the PDESystem
+#     Model.@named pdesys = Model.PDESystem(eqs, bcs, domains, [vars], vars_symb);
 
-    # Discretization
-    discretization = MOLFiniteDifference([x => stepx], nothing, approx_order=2);
+#     # Discretization
+#     discretization = MOLFiniteDifference([x => stepx], nothing, approx_order=2);
 
-    # Convert to NonlinearProblem
-    sys, tspan = SciMLBase.symbolic_discretize(pdesys, discretization);
+#     # Convert to NonlinearProblem
+#     sys, tspan = SciMLBase.symbolic_discretize(pdesys, discretization);
     
+#     sys = Model.complete(sys)
+    
+#     prob = NonlinearProblem(sys, ones(length(Model.get_eqs(sys))); discretization.kwargs...);
+    
+#     # Remake and solve
+#     prob_new = remake(prob, u0=x0)
+#     sol = NonlinearSolve.solve(prob_new, NewtonRaphson(), reltol=1e-5, abstol=1e-5)
+
+#     solution_coeffs = []
+#     for i in 1:(2*harmonics)
+#         # Extract solution    
+#         push!(solution_coeffs, sol[vars_symb[i]]);
+#     end
+
+#     return solution_coeffs
+# end
+
+function solve_harmonicbalance(eqs, harmonics, bcs, domains, steps, vars, vars_symb, x0)
+    # domains: tuple of tuples, e.g., ((xleft, xright),) for 1D or ((xleft, xright), (yleft, yright)) for 2D
+    # steps: tuple of step sizes, e.g., (stepx,) for 1D or (stepx, stepy) for 2D
+    # vars: tuple of spatial variables, e.g., (x,) for 1D or (x, y) for 2D
+    
+    ndims = length(vars)
+    
+    # Build domain specifications
+    domain_specs = [vars[i] ∈ Interval(domains[i]...) for i in 1:ndims]
+    
+    # Create the PDESystem
+    Model.@named pdesys = Model.PDESystem(eqs, bcs, domain_specs, collect(vars), vars_symb)
+    
+    # Build discretization dictionary
+    disc_dict = [vars[i] => steps[i] for i in 1:ndims]
+    discretization = MOLFiniteDifference(disc_dict, nothing, approx_order=2)
+    
+    # Convert to NonlinearProblem
+    sys, tspan = SciMLBase.symbolic_discretize(pdesys, discretization)
     sys = Model.complete(sys)
     
-    prob = NonlinearProblem(sys, ones(length(Model.get_eqs(sys))); discretization.kwargs...);
+    prob = NonlinearProblem(sys, ones(length(Model.get_eqs(sys))); discretization.kwargs...)
     
     # Remake and solve
     prob_new = remake(prob, u0=x0)
     sol = NonlinearSolve.solve(prob_new, NewtonRaphson(), reltol=1e-5, abstol=1e-5)
-
-    solution_coeffs = []
-    for i in 1:(2*harmonics)
-        # Extract solution    
-        push!(solution_coeffs, sol[vars_symb[i]]);
-    end
-
+    
+    # Extract solution coefficients
+    solution_coeffs = [sol[vars_symb[i]] for i in 1:(2*harmonics)]
+    
     return solution_coeffs
 end
 
